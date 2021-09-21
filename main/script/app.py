a6c548f17aa2a694548c40d9243f2b0c7af26b7c
francesco-camaione <francesco.camaione@gmail.com>
2021-09-21 18:22:52 +0200

    Merge branch 'integrazione_mysql' of https://github.com/francesco-camaione/CodiceFiscale into integrazione_mysql

--- main/script/app.py
+++ main/script/app.py
@@ -63,8 +63,8 @@ codice_fiscale = utils.funzione_cognomi(cognome) + utils.funzione_nomi(nome) + y
 dispari1 = codice_fiscale[14].upper() + codice_fiscale[12].upper() + codice_fiscale[10].upper() + \
            codice_fiscale[8].upper() + codice_fiscale[6].upper() + codice_fiscale[4].upper() + \
            codice_fiscale[2].upper() + codice_fiscale[0].upper()
-pari2 = codice_fiscale[13].upper() + codice_fiscale[11].upper() + codice_fiscale[9].upper() +\
-        codice_fiscale[7].upper() + codice_fiscale[5].upper() + codice_fiscale[3].upper() + codice_fiscale[1].upper()
+pari2 = (codice_fiscale[13] + codice_fiscale[11] + codice_fiscale[9] + codice_fiscale[7] + codice_fiscale[5]
+         + codice_fiscale[3] + codice_fiscale[1]).upper()
 valori_disp = (dictionaries.dispari[dispari1[0]] + dictionaries.dispari[dispari1[1]] + dictionaries.dispari[dispari1[2]]
                + dictionaries.dispari[dispari1[3]] + dictionaries.dispari[dispari1[4]]
                + dictionaries.dispari[dispari1[5]] + dictionaries.dispari[dispari1[6]] +
