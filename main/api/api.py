95a850c25dc6b5782e8d4c3106a3ddbb56e614ae
francesco-camaione <francesco.camaione@gmail.com>
2021-09-28 15:25:07 +0200

    .

--- main/api/api.py
+++ main/api/api.py
@@ -8,13 +8,9 @@ import os
 
 app = FastAPI()
 templates = Jinja2Templates(directory="src/build/")
-ON_HEROKU = os.environ.get('ON_HEROKU')
 
-if ON_HEROKU:
-    # get the heroku port
-    port = int(os.environ.get('PORT', 17995))  # as per OP comments default is 17995
-else:
-    port = 3000
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
 
 
 @app.get("/")
